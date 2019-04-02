2db520cb45c27d73c54d46dcebd58c90dd7147ed
Japsz <benjamin.meneses.14@sansano.usm.cl>
2019-04-02 03:51:58 -0300

    Update adminModel.js

--- modelo/adminModel.js
+++ modelo/adminModel.js
@@ -23,7 +23,7 @@ admin.loginHandler = function(username,password,callback){
 admin.getVisitasByEdad = function(db,desde,hasta,callback){
     if(connection){
         connection.query("SELECT  YEAR(visitas.fnac)  AS ano, COUNT(visitas.id) AS visitas,visitas.duration  " +
-            "FROM (SELECT visita.*,jumper.fnac FROM " + db +".visita LEFT JOIN " + db +".jumper ON jumper.id = visita.idjumper GROUP BY visita.id) AS visitas " +
+            "FROM (SELECT visita.*,jumper.fnac FROM ´" + db +"´.visita LEFT JOIN ´" + db +"´.jumper ON jumper.id = visita.idjumper GROUP BY visita.id) AS visitas " +
             "WHERE (visitas.fnac BETWEEN '1900-11-3' AND ?) AND (visitas.date_g BETWEEN ? AND ?)  AND visitas.status = 'ended' " +
             "GROUP BY YEAR(visitas.fnac),visitas.duration",[new Date().toLocaleDateString(),desde,hasta],function(err,rows){
             if(err){
